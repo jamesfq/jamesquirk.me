c133472816ca4aa224b7ab29d74d940cc8b312e3
James Quirk <jamesfq2021@gmail.com>
2023-05-15 19:32:21 -0400

    One last attempt

--- assets/stylesheets/functions.js
+++ assets/stylesheets/functions.js
@@ -1,10 +1,10 @@
 function toggle() {
     var img1 = document.getElementById("morePlanes");
-    if (img1.style.display === "none") {
-        img1.style.display = "block";
+    if (img1.hidden) {
+        img1.hidden = !img1.hidden;
     }
-    
-    else {
-        img1.style.display = "none";
+
+    else  {
+        img1.hidden = !img1.hidden;
     }
 }
