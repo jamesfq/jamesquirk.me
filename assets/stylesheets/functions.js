690d085581b9d21355b031fd5ba2fcfe39c4d24e
James Quirk <jamesfq2021@gmail.com>
2023-05-15 19:24:09 -0400

    New approach to image off

--- assets/stylesheets/functions.js
+++ assets/stylesheets/functions.js
@@ -1,10 +1,4 @@
 function toggle() {
     var img1 = document.getElementById("planeImage");
-    if (img1.hidden) {
-        !img1.hidden;
-    }
-    
-    else {
-        img1.hidden;
-    }
+    img1.hidden = !img1.hidden
 }
