ed16186b797c630a56d6bdbb6810db09a7a1bbd4
James Quirk <jamesfq2021@gmail.com>
2023-05-15 19:12:18 -0400

    Start off

--- assets/stylesheets/functions.js
+++ assets/stylesheets/functions.js
@@ -1,5 +1,6 @@
 function toggle() {
     var img1 = document.getElementById("planeImage");
+    window.print("We're doing something");
     if (img1.style.display === "none") {
       img1.style.display = "block";
     } 
