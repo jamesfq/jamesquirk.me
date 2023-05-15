fb68031f10f49922c4f9226899324682d55b36b9
James Quirk <jamesfq2021@gmail.com>
2023-05-15 19:00:42 -0400

    Add function

---
+++ assets/stylesheets/functions.js
@@ -0,0 +1,8 @@
+function toggle() {
+    var img1 = document.getElementById("planeImage");
+    if (img1.style.display === "none") {
+      img1.style.display = "block";
+    } else {
+      img1.style.display = "none";
+    }
+  }
