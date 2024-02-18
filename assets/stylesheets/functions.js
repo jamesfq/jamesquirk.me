b38f102c2cee1bc0ac54f52b0c4b986d68317d55
James Quirk <jamesfq2021@gmail.com>
2024-02-17 23:20:28 -0500

    Updated butons

--- assets/stylesheets/functions.js
+++ assets/stylesheets/functions.js
@@ -1,15 +1,47 @@
-// my attempt at writing a function to toggle an image when a button was clicked
-function toggle() {
-    // found the rough template for this idea at this URL: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
-    var img1 = document.getElementById("planeImage");
-    
-    // idea was that if the image was marked as hidden
-    if (img1.hidden) {
-        img1.hidden = false; // it would be changed to not hidden
-    }
+const flight1 = document.getElementById("flight1")
+const vid1 = document.getElementById("vid1")
+button1Hidden = true
 
-    // and vice versa
-    else  {
-        img1.hidden = true;
+flight1.addEventListener("click", () => {
+    vid1.classList.toggle("hiddenVideo");
+    if (button1Hidden) {
+        flight1.textContent = "Hide video";
+        button1Hidden = false;
     }
-}
+    else {
+        flight1.textContent = "Solo flight";
+        button1Hidden = true;
+    }
+  });
+
+const flight2 = document.getElementById("flight2")
+const vid2 = document.getElementById("vid2")
+button2Hidden = true
+
+flight2.addEventListener("click", () => {
+    vid2.classList.toggle("hiddenVideo");
+    if (button2Hidden) {
+        flight2.textContent = "Hide video";
+        button2Hidden = false;
+    }
+    else {
+        flight2.textContent = "Night Flight";
+        button2Hidden = true;
+    }
+});
+
+const flight3 = document.getElementById("flight3")
+const vid3 = document.getElementById("vid3")
+button3Hidden = true
+
+flight3.addEventListener("click", () => {
+    vid3.classList.toggle("hiddenVideo");
+    if (button3Hidden) {
+        flight3.textContent = "Hide video";
+        button3Hidden = false;
+    }
+    else {
+        flight3.textContent = "Helicopter";
+        button3Hidden = true;
+    }
+});
